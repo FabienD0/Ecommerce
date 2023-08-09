8d2e6b954f0068f9cd5a7d88b3374cd384d27e26
FabienD0 <fabien.denommee@gmail.com>
2023-08-08 21:09:51 -0400

    sectionOneHomePageDone

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,11 +1,19 @@
+import { styled } from "styled-components"
 import SectionOneHomePage from "../components/SectionHomePage/SectionOneHomePage"
 
 const Homepage = () => {
  return (
-    <>
+    <ContainerAll>
     <SectionOneHomePage />
-    </>
+    </ContainerAll>
  )
 }
 
 export default Homepage
+
+const ContainerAll = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10rem;
+  width: 80rem;
+`;
