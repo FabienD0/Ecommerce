64f6f168d1a77b0eb7eb52527b630a0921a0e5ab
FabienD0 <fabien.denommee@gmail.com>
2023-08-14 21:15:30 -0400

    requestMysql

--- backend/mysqltest.ts
+++ backend/mysqltest.ts
@@ -1,12 +1,24 @@
 require('dotenv').config()
 const mysql = require('mysql2')
+import { RowDataPacket } from "mysql2";
 
-const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 const test = async (req: Request, res: Response) => {
-    const connection = mysql.createConnection(process.env.DATABASE_URL)
-    console.log('Connected to PlanetScale!')
+  try {
+    const connection = await mysql.createConnection(process.env.DATABASE_URL);
+    console.log('Connected to PlanetScale!');
+    connection.query('SELECT * FROM companies', (_err:Error, rows:RowDataPacket[]) => {
+      if(_err) {
+        console.log(_err)
+      } else { 
+        console.log(rows[0])
+        console.log(rows[1])
+      }
+    });
     connection.end()
+  } catch (err) {
+    console.log(err)
+  }
   };
 
 
