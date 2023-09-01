e575271c0dfc97f71555b17dfc6bfc77f1550dd5
FabienD0 <fabien.denommee@gmail.com>
2023-08-31 21:53:15 -0400

    modalCartUpdate

--- frontend/src/components/ItemCartCard.tsx
+++ frontend/src/components/ItemCartCard.tsx
@@ -1,4 +1,6 @@
 import { Item } from "./utils/types"
+import { styled } from "styled-components"
+import { MdDelete} from "react-icons/md"
 
 interface itemProps {
 item: Item
@@ -6,10 +8,68 @@ item: Item
 
 const ItemCartCard: React.FC<itemProps> = ({item}) => {
  return (
-    <>
-    <h1>{item.name}</h1>
-    </>
+    <StyledContainer className="container d-flex">
+    <div className="m-0 p-0 position-relative">
+        <img src={item.imageSrc} alt="Item Image" style={{height:"2.5rem"}}/>
+        <ContainerQuantity>
+            <p>x1</p>
+        </ContainerQuantity>
+    </div>
+    <div>
+        <p>{item.name}</p>
+    </div>
+    <div>
+        <p className="fw-bold">{item.price}</p>
+    </div>
+    <DeleteIcon />
+    </StyledContainer>
  )
 }
 
 export default ItemCartCard
+
+const StyledContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  background-color: white;
+  height: 8rem;
+  width: 90%;
+  padding: 2rem 1rem;
+  text-decoration: none;
+  color: black;
+
+  text-align: center;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+    rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+`
+
+const ContainerQuantity = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  bottom: -20px;
+  font-weight: 900;
+  border-radius: 50%;
+  font-size: 12px;
+  width: 1.5rem;
+  height: 1.5rem;
+  opacity: 0.7;
+`;
+
+const DeleteIcon = styled(MdDelete)`
+  position: absolute;
+  font-size: 1.3rem;
+  bottom: 5px;
+  right: 10px;
+  opacity: 0.6;
+
+  &:hover {
+    cursor: pointer;
+    opacity: 1;
+    transition: all 0.3s ease-in-out;
+    -webkit-transition: all 0.3s;
+  }
+`
--- frontend/src/components/modals/Cart.tsx
+++ frontend/src/components/modals/Cart.tsx
@@ -7,6 +7,7 @@ import { useAppDispatch, useAppSelector } from "../../redux/app/hooks";
 import { Item } from "../utils/types"
 import { getOneItem } from "../../redux/features/itemsSlice";
 import ItemCartCard from "../ItemCartCard";
+import { colors } from "../../assets/colors";
 
 interface cartProps {
     $cart: boolean;
@@ -33,9 +34,6 @@ useEffect(() => {
     setItem(itemReturn);
   },[oneItem])
 
-  console.log(item)
-
-
 
   /* Close Cart */ 
   const handleClose = () => {
@@ -48,7 +46,7 @@ useEffect(() => {
   return (
     <Container $cart={isCart}>
       <ContainerTop>
-        <H2>Shopping Cart(2)</H2>
+        <H2 className="fw-bold">Shopping Cart(2)</H2>
         <button style={{ all: "unset",cursor:"pointer" }} onClick={handleClose}>
           <GrClose />
         </button>
@@ -58,7 +56,13 @@ useEffect(() => {
           <H2>You cart is empty</H2>
         </ContainerMid>}
         <ContainerItemCart>
-        {item && <ItemCartCard item={item} />}
+        {item && <>
+          <ItemCartCard item={item} />
+          <ItemCartCard item={item} />
+          <ItemCartCard item={item} />
+        </>
+        }
+        <ClearButton>Clear</ClearButton>
         </ContainerItemCart>
     </Container>
   );
@@ -113,67 +117,25 @@ const ContainerItemCart = styled.div`
   padding-bottom: 1rem;
 `;
 
-const ContainerCheckout = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5em;
-  border-top: 3px dashed black;
-  padding: 1rem 0;
-`;
-
-const ContainerCheckoutButton = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
 const Image = styled.img`
   width: 20rem;
   margin-bottom: 5rem;
 `;
 
-const Button = styled.button`
-  all: unset;
-  width: 12rem;
-  text-align: center;
-  font-weight: 700;
-  border: 2px solid black;
-  letter-spacing: 0.05em;
+const ClearButton = styled.button`
+border: none;
+color: white;
+font-weight: bold;
+border-radius: 5px;
+box-shadow: rgba(0, 0, 0, 0.35) 0px 3px 10px;
+background-color: #fc4646;
+width: 7rem;
+padding: 0.3rem;
+opacity: 0.8;
+transition: opacity .2s ease-in-out;
 
-  color: black;
-  padding: 0.8rem 0;
-  background-color: transparent;
-
-  :hover {
-    cursor: pointer;
-    background-color: black;
-    color: white;
-    transition: all 0.2s;
-    -webkit-transition: all 0.2s;
-  }
-  &:disabled {
-    opacity: 0.2;
-    cursor: default;
-
-    :hover {
-      background-color: transparent;
-      color: black;
-    }
-  }
-`;
-const ClearAllButton = styled.button`
-  color: white;
-  background-color: red;
-  font-weight: 900;
-  width: 7rem;
-  padding: 0.3rem;
-  opacity: 0.7;
-  border-radius: 10px;
-
-  :hover {
-    cursor: pointer;
-    opacity: 1;
-    transition: all 0.3s ease-in-out;
-    -webkit-transition: all 0.3s;
-  }
-`;
+&:hover {
+  box-shadow: none;
+  opacity: 1 !important;
+}
+`
