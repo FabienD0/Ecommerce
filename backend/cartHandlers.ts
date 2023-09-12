5f13da23826837bd451e2dc7f97f8fbedbda24c9
FabienD0 <fabien.denommee@gmail.com>
2023-09-12 19:49:37 -0400

    cartHandlersAddCustomerItems

--- backend/cartHandlers.ts
+++ backend/cartHandlers.ts
@@ -9,13 +9,12 @@ const checkoutCart = async (req: Request, res: Response) => {
 
 const { fname,lname,address,email,cardNumber,expiry,cart } = req.body.cart;
 
-console.log(cart);
-
+//SQL Data for Customers
 const valuesCustomer = [uuidv4(), fname, lname, address, email, cardNumber, expiry];
 const insertCustomer = `INSERT INTO customers (id, firstName, lastName, address, email, cardNumber, expiry)
 VALUES (?, ?, ?, ?, ?, ?, ?)`;
 
-const valuesItems = [cart.id,"",cart.quantity,cart.price];
+//SQL Data for Orders
 const insertItems = `INSERT INTO orders (itemId, customerId, quantity, price)
 VALUES (?, ?, ?, ?)`;
 
@@ -24,20 +23,79 @@ VALUES (?, ?, ?, ?)`;
 try {
   const connection = await mysql.createConnection(process.env.DATABASE_URL);
   connection.query(`SELECT * FROM customers WHERE email = "${email}"`, (_err:Error, rows:RowDataPacket[]) => {
-    console.log(rows[0].id)
 
+    //If customer exist, add order to the DB with current customer ID
     if(rows.length > 0) {
-    console.log("customer exist")
-     connection.end()
+    
+    const queryPromises = [];
 
+    //loop through the cart
+    for(let i=0;i<cart.length;i++) {
+    const valuesItems = [cart[i].id,rows[0].id,cart[i].quantity,cart[i].price];
+
+  // Create a promise for each query.
+  const queryPromise = new Promise((resolve, reject) => {
+    connection.query(insertItems, valuesItems, (err: Error, insertRow: RowDataPacket[]) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(insertRow);
+      }
+    });
+  });
+  queryPromises.push(queryPromise);
+  }
+  // Execute all queries and handle the results.
+  Promise.all(queryPromises)
+  .then((results) => {
+    res.status(200).json({ status: 200, rows: results, message: "Orders added" });
+    connection.end();
+  })
+  .catch((err) => {
+    res.status(500).json({ status: 500, err: err, message: "Error adding orders" });
+    connection.end();
+  });
+
+
+
+
+
+  //If customer doesn't exist, add customer then add order with customer ID
     } else { 
-      connection.query(insertCustomer,valuesCustomer, (_err:Error, insertRow:RowDataPacket[]) => {
+      connection.query(insertCustomer,valuesCustomer, (_err:Error, insertCustomerRow:RowDataPacket[]) => {
         if (_err) {
           res.status(500).json({ status: 500, message: "Error adding customer" });
           connection.end()
         } else {
-          res.status(200).json({ status: 200, rows: insertRow, message: "Customer added" });
-          connection.end()
+
+          const queryPromises = [];
+
+          //loop through the cart
+          for(let i=0;i<cart.length;i++) {
+          const valuesItems = [cart.id,valuesCustomer[0].id,cart.quantity,cart.price];
+      
+        // Create a promise for each query.
+        const queryPromise = new Promise((resolve, reject) => {
+          connection.query(insertItems, valuesItems, (err: Error, insertRow: RowDataPacket[]) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(insertRow);
+            }
+          });
+        });
+        queryPromises.push(queryPromise);
+        }
+        // Execute all queries and handle the results.
+        Promise.all(queryPromises)
+        .then((results) => {
+          res.status(200).json({ status: 200, rows: results, message: "Orders & Customer added" });
+          connection.end();
+        })
+        .catch((err) => {
+          res.status(500).json({ status: 500, err: err, message: "Error adding orders" });
+          connection.end();
+        });
         }
     })}
   });
